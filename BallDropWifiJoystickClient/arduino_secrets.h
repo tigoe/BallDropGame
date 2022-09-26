a5e7557df31de75a16daf1f46bad35f3692de748
Tom Igoe <tom.igoe@gmail.com>
2022-09-26 08:37:56 -0400

    Update joystick clients

--- BallDropWifiJoystickClient/arduino_secrets.h
+++ BallDropWifiJoystickClient/arduino_secrets.h
@@ -1,2 +1,2 @@
-#define SECRET_SSID "sandbox370" //  your network SSID (name)
-#define SECRET_PASS  "+s0a+s03!2gether?"    // your network password (use for WPA, or use as key for WEP)
+#define SECRET_SSID "" //  your network SSID (name)
+#define SECRET_PASS  ""    // your network password (use for WPA, or use as key for WEP)
